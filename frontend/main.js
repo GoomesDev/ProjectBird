26602f65ac220918d084f8b9ede6b6e79fd7f281
GoomesDev <henriquegomesdev@gmail.com>
2024-01-07 02:02:28 -0300

    First Commit

---
+++ frontend/main.js
@@ -0,0 +1,40 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev');
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+    autoHideMenuBar: true
+  });
+
+  mainWindow.loadURL(
+    isDev
+      ? 'http://localhost:3000/login'
+      : `file://${path.join(__dirname, '../build/pages/login/index.html')}`
+  );
+
+  mainWindow.on('closed', () => {
+    mainWindow = null;
+  });
+}
+
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
---
+++ frontend/pages/login/index.jsx
@@ -0,0 +1,12 @@
+import { Paper } from "@mui/material"
+
+const LoginPage = () => {
+
+    return (
+        <Paper>
+            <h1>Teste</h1>
+        </Paper>
+    )
+}
+
+export default LoginPage
